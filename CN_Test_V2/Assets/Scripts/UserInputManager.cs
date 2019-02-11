25e67591f517b9fd411ffef9260259eed0f83608
ColetteN <k00212038@student.lit.ie>
2019-02-11 15:24:54 +0000

    Creating the top input section

---
+++ CN_Test_V2/Assets/Scripts/UserInputManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInputManager : MonoBehaviour {
+
+	public void ClickButton(){
+		Debug.Log("Button Clicked");
+	}
+}
