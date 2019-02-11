a7e8929e68ce437431d22c333336957c40296c15
ColetteN <k00212038@student.lit.ie>
2019-02-11 14:51:59 +0000

    Grid space and image arrays created

---
+++ CN_Test_V2/Assets/Scripts/GridHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridHandler : MonoBehaviour {
+
+	//Image (public)
+	//Public so we can drag and drop in inspector
+	//ref to the image only
+	public Sprite[] testImage;
+
+	//Grid (public)
+	//Public so we can drag and drop in inspector
+	//ref to the grid only
+	//the UI image element
+	public Image[] gridSpace;
+
+	// Use this for initialization
+	void Start () {
+		//Put image into grid
+		//gridSpace sprite property setting equal to new image
+		for (int i = 0; i < testImage.Length; i++)
+        {
+			//each image into each gridspace
+            gridSpace[i].sprite = testImage[i];
+        }
+	}
+	
+	
+}
+
+
+
